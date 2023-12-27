ecf095e10e81dd8f10880277559a1322af22ee17
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2023-12-27 14:31:29 +0200

    feat(cosmosutil): add querying methods for the Auth module

---
+++ cosmosutil/auth.go
@@ -0,0 +1,33 @@
+package cosmosutil
+
+import (
+	"context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+)
+
+func (c *ChainClient) Account(ctx context.Context, address string) (sdk.AccountI, error) {
+	authClient, err := c.getAuthClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := authClient.Account(ctx, &authtypes.QueryAccountRequest{
+		Address: address,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var acc authtypes.BaseAccount
+
+	err = c.EncodingConfig.InterfaceRegistry.UnpackAny(res.Account, &acc)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &acc, nil
+}
