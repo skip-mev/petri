898b9279497c3867af35efd0d4a8346b064442a2
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2024-01-02 19:57:36 +0200

    feat(cosmosutil): some context additions and fixes

--- cosmosutil/client.go
+++ cosmosutil/client.go
@@ -1,8 +1,10 @@
 package cosmosutil
 
 import (
+	"context"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	petritypes "github.com/skip-mev/petri/types"
 )
 
@@ -14,8 +16,8 @@ type ChainClient struct {
 // todo: remove the nolints
 
 //nolint:unused
-func (c *ChainClient) getAuthClient() (authtypes.QueryClient, error) {
-	cc, err := c.Chain.GetGPRCClient()
+func (c *ChainClient) getAuthClient(ctx context.Context) (authtypes.QueryClient, error) {
+	cc, err := c.Chain.GetGRPCClient(ctx)
 
 	if err != nil {
 		return nil, err
@@ -25,12 +27,23 @@ func (c *ChainClient) getAuthClient() (authtypes.QueryClient, error) {
 }
 
 //nolint:unused
-func (c *ChainClient) getBankClient() (banktypes.QueryClient, error) {
-	cc, err := c.Chain.GetGPRCClient()
+func (c *ChainClient) getBankClient(ctx context.Context) (banktypes.QueryClient, error) {
+	cc, err := c.Chain.GetGRPCClient(ctx)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return banktypes.NewQueryClient(cc), nil
+}
+
+//nolint:unused
+func (c *ChainClient) getGovClient(ctx context.Context) (govtypes.QueryClient, error) {
+	cc, err := c.Chain.GetGRPCClient(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return govtypes.NewQueryClient(cc), nil
 }
