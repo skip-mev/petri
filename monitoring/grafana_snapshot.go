5c4e52785e591c79e70fa77ff14c78d4f0534d42
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2024-01-08 13:17:55 +0100

    feat: add monitoring module

---
+++ monitoring/grafana_snapshot.go
@@ -0,0 +1,147 @@
+package monitoring
+
+import (
+	"context"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/rod/lib/proto"
+	"time"
+)
+
+func SnapshotGrafanaDashboard(ctx context.Context, uid, grafanaURL string) (string, error) {
+	url, err := launcher.New().Launch()
+
+	if err != nil {
+		return "", err
+	}
+
+	browser := rod.New().ControlURL(url)
+
+	err = browser.Connect()
+
+	if err != nil {
+		return "", err
+	}
+
+	defer browser.MustClose()
+
+	loginPage, err := browser.Page(proto.TargetCreateTarget{URL: grafanaURL + "/login"})
+
+	if err != nil {
+		return "", err
+	}
+
+	err = loginPage.WaitLoad()
+
+	if err != nil {
+		return "", err
+	}
+
+	usernameInput, err := loginPage.Element("input[name='user']")
+
+	if err != nil {
+		return "", err
+	}
+
+	err = usernameInput.Input("admin")
+
+	if err != nil {
+		return "", err
+	}
+
+	passwordInput, err := loginPage.Element("input[name='password']")
+
+	if err != nil {
+		return "", err
+	}
+
+	err = passwordInput.Input("admin")
+
+	if err != nil {
+		return "", err
+	}
+
+	submitButton, err := loginPage.Element("button[type='submit']")
+
+	if err != nil {
+		return "", err
+	}
+
+	err = submitButton.Click(proto.InputMouseButtonLeft, 1)
+
+	if err != nil {
+		return "", err
+	}
+
+	time.Sleep(time.Second)
+
+	page, err := browser.Page(proto.TargetCreateTarget{URL: grafanaURL + "/d/" + uid})
+
+	if err != nil {
+		return "", err
+	}
+
+	err = page.WaitLoad()
+
+	if err != nil {
+		return "", err
+	}
+
+	shareDashboardButton, err := page.Element("[aria-label='Share dashboard']")
+
+	if err != nil {
+		return "", err
+	}
+
+	err = shareDashboardButton.Click(proto.InputMouseButtonLeft, 1)
+
+	if err != nil {
+		return "", err
+	}
+
+	snapshotButton, err := page.Element("[aria-label='Tab Snapshot']")
+
+	if err != nil {
+		return "", err
+	}
+
+	err = snapshotButton.Click(proto.InputMouseButtonLeft, 1)
+
+	if err != nil {
+		return "", err
+	}
+
+	publishButton, err := page.ElementR("span", "Publish to snapshots.raintank.io")
+
+	if err != nil {
+		return "", err
+	}
+
+	err = publishButton.Click(proto.InputMouseButtonLeft, 1)
+
+	if err != nil {
+		return "", err
+	}
+
+	// wait for the snapshot to be created
+
+	err = page.WaitElementsMoreThan("input[id='snapshot-url-input']", 0)
+
+	if err != nil {
+		return "", err
+	}
+
+	snapshotURLInput, err := page.Element("input[id='snapshot-url-input']")
+
+	if err != nil {
+		return "", err
+	}
+
+	snapshotURL, err := snapshotURLInput.Attribute("value")
+
+	if err != nil {
+		return "", err
+	}
+
+	return *snapshotURL, err
+}
