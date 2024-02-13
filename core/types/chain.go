84727842fa68c92d2f006114fe432e558aaefea0
Nikhil Vasan <nikhil@skip.money>
2024-02-13 15:22:26 -0800

    fixes for dydx loadtests
    (cherry picked from commit 08711a7ef3d7fd65045fa9fc60e53327540e90ea)

--- core/types/chain.go
+++ core/types/chain.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"math/big"
-
 	rpchttp "github.com/cometbft/cometbft/rpc/client/http"
 	"github.com/cosmos/cosmos-sdk/client"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
