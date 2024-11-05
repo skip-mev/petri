9d90aec64903e7323965d4abd664bebfa0275a40
Eric Warehime <eric.warehime@gmail.com>
2024-11-05 10:54:04 -0800

    Move log to debug

--- core/provider/digitalocean/digitalocean_provider.go
+++ core/provider/digitalocean/digitalocean_provider.go
@@ -70,7 +70,7 @@ func NewDigitalOceanProvider(ctx context.Context, logger *zap.Logger, providerNa
 		sshFingerprint: sshFingerprint,
 	}
 
-	logger.Info("petri tag", zap.String("tag", digitalOceanProvider.petriTag))
+	logger.Debug("petri tag", zap.String("tag", digitalOceanProvider.petriTag))
 
 	_, err = digitalOceanProvider.createTag(ctx, digitalOceanProvider.petriTag)
 
