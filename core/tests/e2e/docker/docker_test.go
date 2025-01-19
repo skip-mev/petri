ae78d385723ac847aec3aae7ad1a064551989ce2
nadimabdelaziz <nadeem.abdelaziz99@gmail.com>
2025-01-19 06:44:15 +0200

    test: e2e docker test

--- core/tests/e2e/docker/docker_test.go
+++ core/tests/e2e/docker/docker_test.go
@@ -3,6 +3,9 @@ package e2e
 import (
 	"context"
 	"flag"
+	"fmt"
+	"os"
+	"sync"
 	"testing"
 
 	"github.com/skip-mev/petri/core/v2/tests/e2e"
@@ -12,7 +15,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
+	gonanoid "github.com/matoous/go-nanoid/v2"
 	"github.com/skip-mev/petri/core/v2/provider"
+	"github.com/skip-mev/petri/core/v2/provider/digitalocean"
 	"github.com/skip-mev/petri/core/v2/provider/docker"
 	"github.com/skip-mev/petri/core/v2/types"
 	cosmoschain "github.com/skip-mev/petri/cosmos/v2/chain"
@@ -49,6 +54,17 @@ var (
 			DerivationFn:     hd.Secp256k1.Derive(),
 			GenerationFn:     hd.Secp256k1.Generate(),
 		},
+		NodeOptions: types.NodeOptions{
+			NodeDefinitionModifier: func(def provider.TaskDefinition, nodeConfig types.NodeConfig) provider.TaskDefinition {
+				doConfig := digitalocean.DigitalOceanTaskConfig{
+					"size":     "s-2vcpu-4gb",
+					"region":   "ams3",
+					"image_id": os.Getenv("DO_IMAGE_ID"),
+				}
+				def.ProviderSpecificConfig = doConfig
+				return def
+			},
+		},
 	}
 
 	numTestChains = flag.Int("num-chains", 3, "number of chains to create for concurrent testing")
@@ -63,8 +79,13 @@ func TestDockerE2E(t *testing.T) {
 
 	ctx := context.Background()
 	logger, _ := zap.NewDevelopment()
+	providerName := gonanoid.MustGenerate("abcdefghijklqmnoqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 10)
+
+	p, err := docker.CreateProvider(ctx, logger, providerName)
+	require.NoError(t, err)
 
 	defer func() {
+		require.NoError(t, p.Teardown(ctx))
 		dockerClient, err := client.NewClientWithOpts()
 		if err != nil {
 			t.Logf("Failed to create Docker client for volume cleanup: %v", err)
