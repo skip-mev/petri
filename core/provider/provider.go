ac5e4acbe4b4dfa6bd23e624d9f51e47b4605d90
nadimabdelaziz <nadeem.abdelaziz99@gmail.com>
2025-02-27 23:12:34 +0200

    chore: cleanup unused files
    (cherry picked from commit a6b45ec231690f76136edace2d063e26dbcaa7a0)
    
    # Conflicts:
    #	core/provider/digitalocean/task.go
    #	core/provider/provider.go
    #	core/provider/task.go

--- core/provider/provider.go
+++ core/provider/provider.go
@@ -2,8 +2,12 @@ package provider
 
 import (
 	"context"
+<<<<<<< HEAD
 	"go.uber.org/zap"
 	"sync"
+=======
+	"net"
+>>>>>>> a6b45ec (chore: cleanup unused files)
 )
 
 // TaskStatus defines the status of a task's underlying workload
@@ -16,6 +20,7 @@ const (
 	TASK_PAUSED
 )
 
+<<<<<<< HEAD
 // Task is a stateful object that holds the underlying workload's details and tracks the workload's lifecycle
 type Task struct {
 	Provider Provider
@@ -51,4 +56,36 @@ type Provider interface {
 
 	RunCommand(context.Context, string, []string) (string, string, int, error)
 	RunCommandWhileStopped(context.Context, string, TaskDefinition, []string) (string, string, int, error)
+=======
+type TaskI interface {
+	Start(context.Context) error
+	Stop(context.Context) error
+	Destroy(context.Context) error
+
+	GetDefinition() TaskDefinition
+
+	GetStatus(context.Context) (TaskStatus, error)
+
+	Modify(context.Context, TaskDefinition) error
+
+	WriteFile(context.Context, string, []byte) error
+	ReadFile(context.Context, string) ([]byte, error)
+	DownloadDir(context.Context, string, string) error
+
+	GetIP(context.Context) (string, error)
+	GetExternalAddress(context.Context, string) (string, error)
+	DialContext() func(context.Context, string, string) (net.Conn, error)
+
+	RunCommand(context.Context, []string) (string, string, int, error)
+}
+
+type ProviderI interface {
+	CreateTask(context.Context, TaskDefinition) (TaskI, error) // should this create a TaskI or the resources behind it?
+	SerializeTask(context.Context, TaskI) ([]byte, error)
+	DeserializeTask(context.Context, []byte) (TaskI, error)
+
+	Teardown(context.Context) error
+
+	SerializeProvider(context.Context) ([]byte, error)
+>>>>>>> a6b45ec (chore: cleanup unused files)
 }
