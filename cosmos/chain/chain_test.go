d54472c932cacef51214f2535ad2f848a9f3c300
nadimabdelaziz <nadeem.abdelaziz99@gmail.com>
2025-05-28 01:19:00 -0400

    refactor gaia to unit test

--- cosmos/chain/chain_test.go
+++ cosmos/chain/chain_test.go
@@ -51,6 +51,116 @@ var defaultChainOptions = types.ChainOptions{
 	},
 }
 
+var evmChainConfig = types.ChainConfig{
+	Denom:         "atest",
+	Decimals:      6,
+	NumValidators: 1,
+	NumNodes:      1,
+	BinaryName:    "gaiad",
+	Image: provider.ImageDefinition{
+		Image: "ghcr.io/cosmos/gaia:na-build-arm64",
+		UID:   "1025",
+		GID:   "1025",
+	},
+	GasPrices:            "0.0005atest",
+	Bech32Prefix:         "cosmos",
+	HomeDir:              "/gaia",
+	CoinType:             "118",
+	ChainId:              "cosmos_22222-1",
+	UseGenesisSubCommand: true,
+}
+
+var evmChainOptions = types.ChainOptions{
+	NodeCreator: node.CreateNode,
+	WalletConfig: types.WalletConfig{
+		SigningAlgorithm: string(hd.Secp256k1.Name()),
+		Bech32Prefix:     "cosmos",
+		HDPath:           hd.CreateHDPath(118, 0, 0),
+		DerivationFn:     hd.Secp256k1.Derive(),
+		GenerationFn:     hd.Secp256k1.Generate(),
+	},
+	ModifyGenesis: chain.ModifyGenesis([]chain.GenesisKV{
+		{
+			Key:   "app_state.staking.params.bond_denom",
+			Value: "atest",
+		},
+		{
+			Key:   "app_state.gov.deposit_params.min_deposit.0.denom",
+			Value: "atest",
+		},
+		{
+			Key:   "app_state.gov.params.min_deposit.0.denom",
+			Value: "atest",
+		},
+		{
+			Key:   "app_state.evm.params.evm_denom",
+			Value: "atest",
+		},
+		{
+			Key:   "app_state.mint.params.mint_denom",
+			Value: "atest",
+		},
+		{
+			Key: "app_state.bank.denom_metadata",
+			Value: []map[string]interface{}{
+				{
+					"description": "The native staking token for evmd.",
+					"denom_units": []map[string]interface{}{
+						{
+							"denom":    "atest",
+							"exponent": 0,
+							"aliases":  []string{"attotest"},
+						},
+						{
+							"denom":    "test",
+							"exponent": 18,
+							"aliases":  []string{},
+						},
+					},
+					"base":     "atest",
+					"display":  "test",
+					"name":     "Test Token",
+					"symbol":   "TEST",
+					"uri":      "",
+					"uri_hash": "",
+				},
+			},
+		},
+		{
+			Key: "app_state.evm.params.active_static_precompiles",
+			Value: []string{
+				"0x0000000000000000000000000000000000000100",
+				"0x0000000000000000000000000000000000000400",
+				"0x0000000000000000000000000000000000000800",
+				"0x0000000000000000000000000000000000000801",
+				"0x0000000000000000000000000000000000000802",
+				"0x0000000000000000000000000000000000000803",
+				"0x0000000000000000000000000000000000000804",
+				"0x0000000000000000000000000000000000000805",
+			},
+		},
+		{
+			Key:   "app_state.erc20.params.native_precompiles",
+			Value: []string{"0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"},
+		},
+		{
+			Key: "app_state.erc20.token_pairs",
+			Value: []map[string]interface{}{
+				{
+					"contract_owner": 1,
+					"erc20_address":  "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+					"denom":          "atest",
+					"enabled":        true,
+				},
+			},
+		},
+		{
+			Key:   "consensus.params.block.max_gas",
+			Value: "75000000",
+		},
+	}),
+}
+
 func TestChainLifecycle(t *testing.T) {
 	ctx := context.Background()
 	logger, _ := zap.NewDevelopment()
@@ -178,4 +288,33 @@ func TestGenesisModifier(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Contains(t, string(bz), chainName)
+}
+
+func TestGaiaEvm(t *testing.T) {
+	ctx := context.Background()
+	logger, _ := zap.NewDevelopment()
+	providerName := gonanoid.MustGenerate(idAlphabet, 10)
+	chainName := gonanoid.MustGenerate(idAlphabet, 5)
+
+	p, err := docker.CreateProvider(ctx, logger, providerName)
+	require.NoError(t, err)
+	defer func(p provider.ProviderI, ctx context.Context) {
+		require.NoError(t, p.Teardown(ctx))
+	}(p, ctx)
+
+	chainConfig := evmChainConfig
+	chainConfig.Name = chainName
+
+	c, err := chain.CreateChain(ctx, logger, p, chainConfig, evmChainOptions)
+	require.NoError(t, err)
+
+	require.NoError(t, c.Init(ctx, evmChainOptions))
+	require.Len(t, c.GetValidators(), 1)
+	require.Len(t, c.GetNodes(), 1)
+
+	time.Sleep(1 * time.Second)
+
+	require.NoError(t, c.WaitForBlocks(ctx, 2))
+
+	require.NoError(t, c.Teardown(ctx))
 }
